b6a6a646d929743023afafca0d0c7893c8b054d4
Tamino Martinius <io@tamino.dev>
2026-04-03 21:28:40 +0200

    Repo rename

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -6,15 +6,13 @@ export const Footer: FC = memo(() => {
     <footer className="footer">
       <div className="footer__content">
         <div className="footer__left">
-          <a href="https://taminomartinius.de">Made with ❤ by Tamino Martinius</a>
+          <a href="https://tamino.dev">Made with ❤ by Tamino Martinius</a>
         </div>
         <div className="footer__center">
-          <a href="https://github.com/tamino-martinius/contributions.taminomartinius.de">
-            Open Source - Code available at GitHub
-          </a>
+          <a href="https://github.com/tamino-martinius/metrics.tamino.dev">Open Source - Code available at GitHub</a>
         </div>
         <div className="footer__right">
-          <a href="https://taminomartinius.de/#legal">Legal</a>
+          <a href="https://tamino.dev/#legal">Legal</a>
         </div>
       </div>
     </footer>
