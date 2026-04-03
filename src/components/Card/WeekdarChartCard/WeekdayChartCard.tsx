45dbc37d132371f2609e52caaf02317aae20fb63
Tamino Martinius <io@tamino.dev>
2026-04-03 21:47:12 +0200

    Add changed files and change type

--- src/components/Card/WeekdarChartCard/WeekdayChartCard.tsx
+++ src/components/Card/WeekdarChartCard/WeekdayChartCard.tsx
@@ -1,23 +1,50 @@
-import { type FC, memo } from 'react';
+import { type FC, useState } from 'react';
 import { Chart, ChartType } from '@/components/shared/Chart';
-import { HOUR_KEYS, VISIBILITIES, VISIBILITY_TITLES, WEEKDAY_TITLES_LONG } from '@/constants';
+import {
+  CHANGE_TYPE_TITLES,
+  CHANGE_TYPES,
+  HOUR_KEYS,
+  VISIBILITIES,
+  VISIBILITY_TITLES,
+  WEEKDAY_TITLES_LONG,
+} from '@/constants';
 import type Data from '@/models/Data';
 import type { Graph } from '@/types/ComponentStats';
-import { visibilityCommitKey } from '@/util/recordKey';
+import { changeTypeCommitKey, visibilityChangedFilesKey, visibilityCommitKey } from '@/util/recordKey';
 import './WeekdayChartCard.css';
+import { ButtonGroup } from '@/components/shared/ButtonGroup';
 
 interface WeekdayChartCardProps {
   data: Data;
 }
 
-export const WeekdayChartCard: FC<WeekdayChartCardProps> = memo(({ data }) => {
+const MODES = ['Change Type', 'Changed Files', 'Commits'] as const;
+type Mode = (typeof MODES)[number];
+
+export const WeekdayChartCard: FC<WeekdayChartCardProps> = ({ data }) => {
   const { commitStatsPerHour, commitStatTotals } = data;
-  const graphs: Graph[] = VISIBILITIES.map((type, i) => ({
-    title: VISIBILITY_TITLES[i],
-    color: `color-${type}`,
-    value: commitStatTotals[visibilityCommitKey(type)] ?? 0,
-    values: HOUR_KEYS.map((hourKey) => commitStatsPerHour[hourKey]?.[visibilityCommitKey(type)] ?? 0),
-  }));
+
+  const [mode, setMode] = useState<Mode>(MODES[MODES.length - 1]);
+
+  const graphs: Graph[] =
+    mode === 'Commits' || mode === 'Changed Files'
+      ? VISIBILITIES.map((type, i) => ({
+          title: VISIBILITY_TITLES[i],
+          color: `color-${type}`,
+          value: commitStatTotals[(mode === 'Commits' ? visibilityCommitKey : visibilityChangedFilesKey)(type)] ?? 0,
+          values: HOUR_KEYS.map(
+            (hourKey) =>
+              commitStatsPerHour[hourKey]?.[
+                (mode === 'Commits' ? visibilityCommitKey : visibilityChangedFilesKey)(type)
+              ] ?? 0,
+          ),
+        }))
+      : CHANGE_TYPES.map((changeType, i) => ({
+          title: CHANGE_TYPE_TITLES[i],
+          color: `color-${changeType}`,
+          value: commitStatTotals[changeTypeCommitKey(changeType)],
+          values: HOUR_KEYS.map((hourKey) => commitStatsPerHour[hourKey]?.[changeTypeCommitKey(changeType)] ?? 0),
+        }));
 
   return (
     <Chart
@@ -26,6 +53,7 @@ export const WeekdayChartCard: FC<WeekdayChartCardProps> = memo(({ data }) => {
       type={ChartType.COMPARE}
       graphs={graphs}
       xLabels={WEEKDAY_TITLES_LONG}
+      titleSlot={<ButtonGroup labels={MODES} values={[...MODES]} onValueChanged={setMode} />}
     />
   );
-});
+};
